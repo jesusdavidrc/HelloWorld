b18f194261d10d57a9cc1d2dc8ba9224715daa8d
Jesus David Rodriguez Castillo <jesusdavid.rodriguez@globant.com>
2022-01-14 11:07:21 -0500

    some-lines

--- src/com/company/Product1ToN.java
+++ src/com/company/Product1ToN.java
@@ -3,5 +3,16 @@ package com.company;
 public class Product1ToN {
     public static void main(String[] args) {
 
+        long product = 1;
+        final int LOWERBOUND = 1;
+        final int UPPERBOUND = 14;
+        
+        for (int i = LOWERBOUND ; i <= UPPERBOUND; ++i){
+            System.out.println("i = " + i);
+            product *= i;
+
+            System.out.println("product = " + product);
+        }
+
     }
 }
