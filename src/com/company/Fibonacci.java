241bd57d051081e2f11c71595c5b90763632f3df
Jesus David Rodriguez Castillo <jesusdavid.rodriguez@globant.com>
2022-01-20 09:34:34 -0500

    finacci incomplete

---
+++ src/com/company/Fibonacci.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 3;          // The index n for F(n), starting from n=3, as n=1 and n=2 are pre-defined
+        int fn = 0;             // F(n) to be computed
+        int fnMinus1 = 1;   // F(n-1), init to F(2)
+        int fnMinus2 = 1;   // F(n-2), init to F(1)
+        int nMax = 20;      // maximum n, inclusive
+        int sum = fnMinus1 + fnMinus2;  // Need sum to compute average
+        double average = 0.0;
+
+        System.out.print("The first " + nMax + " Fibonacci numbers are: \n"+fnMinus1+" " + fnMinus2);
+
+
+        while (n <= nMax) {  // n starts from 3
+            // n = 3, 4, 5, ..., nMax
+            // Compute F(n), print it and add to sum
+            fn = fnMinus1 + fnMinus2;
+
+            // Increment the index n and shift the numbers for the next iteration
+            ++n;
+            fnMinus2 = fnMinus1;
+            fnMinus1 = fn;
+
+            System.out.print(" "+fn);
+
+        }
+
+        //average = (double)fn / (double)nMax;
+      //  System.out.println("\n average is = " + average);
+
+
+    }
+}
